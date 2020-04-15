17ca78ffbbea03dcef2bfc3725208b44c7df0c0f
Dharana <47414442+Dharana23@users.noreply.github.com>
2020-04-15 11:09:26 +0530

    Add files via upload

---
+++ User-Defined DS/Stack.py
@@ -0,0 +1,22 @@
+stack = []
+def empty():
+    if len(stack) == 0:
+        print('\n Stack is empty')
+    else:
+        print(stack)
+        
+val1 = 1
+val2 = 2
+val3 = 3
+
+stack.append(val1)
+stack.append(val2)
+stack.append(val3)
+empty()
+
+a = stack.pop()
+b = stack.pop()
+c = stack.pop()
+
+print('\n The popped elements are:', a, b, c)
+empty()
