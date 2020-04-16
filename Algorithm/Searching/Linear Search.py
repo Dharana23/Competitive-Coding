fbac98c9ac90d7d1d0dd07473d1a20ea4c920716
Dharana <47414442+Dharana23@users.noreply.github.com>
2020-04-16 14:14:26 +0530

    Add files via upload

---
+++ Algorithm/Searching/Linear Search.py
@@ -0,0 +1,14 @@
+def lin_search(myarray, n, key):
+    for x in range(0, n):
+        if (myarray[x] == key):
+            return x
+    return -1
+
+myarray = [12, 1, 34, 17]
+key = 17
+n = len(myarray)
+matched = lin_search(myarray, n, key)
+if (matched == -1):
+    print('\n Key is not present')
+else:
+    print('\n Key is present at index', matched)
