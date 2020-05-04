a15399a166770a996f086ae9bc7bab79e170e828
Dharana <47414442+Dharana23@users.noreply.github.com>
2020-05-05 00:20:01 +0530

    Add files via upload

---
+++ Python/Built-In-DS/Lists.py
@@ -0,0 +1,35 @@
+list1 = [1, 2, 3, 'Python']
+
+list1.append(2)
+print(list1)
+
+list1.extend((2, 0))
+print(list1)
+
+list1.insert(3, 'example')
+print(list1)
+
+del list1[3]
+print(list1)
+
+list1.pop(3)
+print(list1)
+
+list1.remove(2)
+print(list1)
+
+list = [1, 2, 3, 635, 12, 546, 0]
+
+print(sorted(list))
+
+# Ascending Order
+list.sort()
+print(list)
+
+# Descending Order
+list.sort(reverse=True)
+print(list)
+
+print(list.index(2))
+
+print(list.count(12))
---
+++ Python/Built-In-DS/Tuples.py
@@ -0,0 +1,4 @@
+tuple1 = (1, 2, 3)
+
+tuple1 = tuple1 + (4, 5, 6)
+print(tuple1)
