4f3053182ec685eda0031e3ce330b5ddc364f714
Dharana <47414442+Dharana23@users.noreply.github.com>
2020-05-05 14:07:12 +0530

    Update Functions.cpp

--- C++/Basic C++/Functions.cpp
+++ C++/Basic C++/Functions.cpp
@@ -36,9 +36,9 @@ int max_of_four(int a, int b, int c, int d)
 
 int main() {
     int a, b, c, d;
-    scanf("%d %d %d %d", &a, &b, &c, &d);
+    cin>>a>>b>>c>>d;
     int ans = max_of_four(a, b, c, d);
-    printf("%d", ans);
+    cout<<ans;
     
     return 0;
 }
