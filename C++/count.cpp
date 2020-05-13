21f699e2288db8cf8cc23d4ee9d470037261b117
Dharana <47414442+Dharana23@users.noreply.github.com>
2020-05-14 00:12:41 +0530

    Add files via upload

---
+++ C++/count.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	freopen("countin.txt", "r", stdin);
+	freopen("countout.txt", "w", stdout);
+
+	int n;
+	cin >> n;
+	
+	for (int i=1;i<=n;i++) {
+		cout << i << endl;
+	}	
+	return 0;
+}
